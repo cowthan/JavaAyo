ea6edd417e6fe18c4c4e0465c387e3239b9dc95d
cowthan <cowthan2013@gmail.com>
2016-12-04 22:11:40 +0800

    no update

--- src/com/cowthan/Test.java
+++ src/com/cowthan/Test.java
@@ -1,7 +1,11 @@
 package com.cowthan;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Date;
 import java.util.List;
+
+import android.webkit.URLUtil;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
@@ -12,8 +16,20 @@ public class Test {
 		System.out.println(new Date());
 		
 		
-		List<String> t = JSON.parseObject("[\"aaa\"]", new TypeReference<List<String>>(){});
-		System.out.println(t.size() + "");
+//		List<String> t = JSON.parseObject("[\"aaa\"]", new TypeReference<List<String>>(){});
+//		System.out.println(t.size() + "");
+		
+		String url = "http://www.baidu.com?key={key}";
+		String key = "哈哈";
+		String wholeUrl = "";
+		try {
+			wholeUrl = url.replace("{key}", URLEncoder.encode(key, "utf-8"));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(wholeUrl);
+		
 	}
 }
 
